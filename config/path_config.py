e66343dd30560525af0d948badaa65d8bd237c30
truongnm <truongnm@techcombank.com.vn>
2021-09-11 19:41:24 +0700

    Add Lecture 04: Path config

---
+++ config/path_config.py
@@ -0,0 +1,10 @@
+import os
+
+
+class PathConfig:
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DATA = 'data'
+
+    # File path
+    CHURN_DATA_PATH = 'ChurnData.csv'
+    FUEL_DATA_PATH = 'FuelConsumptionCo2.csv'
